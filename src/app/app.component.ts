619f2edc1aa021f277619f05d4923b5e1f837e3b
pinku1996 <sm24767@gmail.com>
2023-03-23 19:08:30 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'pro2';
+  loginForm = new FormGroup({
+    fname: new FormControl(''),
+    lname: new FormControl(''),
+    Emailaddress: new FormControl(''),
+    Password: new FormControl(''),
+    cars: new FormControl('')
+  });
+  getFormData(data: any) {
+    console.log(data);
+  }
+  santos() {
+    this.router.navigate(['/home'])
+  }
 }
